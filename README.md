bd3519bd9155476b9836d954ea37d744ec87d4a4
WalkerRout <walkerrout04@gmail.com>
2024-07-18 15:01:12 -0700

    Update README

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ KG-RAG Backend
 
 
 #### Bugs
-- Neo4j needs permissions for the .gitignore in the /data directory, must be given with
-  - `sudo chown 1000:1000 ./neo4j/data/.gitignore`
+- Neo4j/Postgres need permissions for the .gitignore in the /data directory, must be given with
+  - `sudo chown 1000:1000 ./neo4j/data/.gitignore` (replacing neo4j with postgres)
--- embedding-generation/generate.py
+++ embedding-generation/generate.py
@@ -3,7 +3,7 @@ import os
 from langchain.text_splitter import TokenTextSplitter
 
 from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.document_loaders import WikipediaLoader, PyPDFLoader
+from langchain_community.document_loaders import WikipediaLoader, PyPDFDirectoryLoader
 from langchain_community.graphs.neo4j_graph import Neo4jGraph
 
 from langchain_experimental.graph_transformers import LLMGraphTransformer
@@ -16,7 +16,7 @@ pdf_path = os.environ["PDF_PATH"]
 # load the documents
 print(f"Loading documents from {pdf_path}") # todo make this logger
 raw_documents = WikipediaLoader(query="Elizabeth I").load()
-# raw_documents = PyPDFLoader(pdf_path).load()
+# raw_documents = PyPDFDirectoryLoader(pdf_path).load()
 
 # chunk documents
 text_splitter = TokenTextSplitter(chunk_size=512, chunk_overlap=24)
